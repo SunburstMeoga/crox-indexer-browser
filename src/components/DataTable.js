2a5379dac92c791085e77d90a0e96590868df40b
SunburstMeoga <1025113986@qq.com>
2024-04-06 16:13:20 +0800

    table

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -27,8 +27,8 @@ const DataTable = ({ tableTitle }) => {
           {titleColumns.map((item, index) => {
             return <div
               key={index}
-              className={['', item.colWidth ? item.colWidth : 'flex-1'].join(" ")}>
-              <div className={['pl-2 text-left h-14 font-bold pt-2 border-word-gray', index !== 0 ? 'border-l' : '', item.titleWidth ? item.titleWidth : ''].join(" ")}>{item.title}</div>
+              className={['', item.colWidth ? item.colWidth : 'flex-1', index !== 0 ? 'border-l border-word-gray' : ''].join(" ")}>
+              <div className={['pl-2 text-left h-14 font-bold pt-2 ', item.titleWidth ? item.titleWidth : ''].join(" ")}>{item.title}</div>
             </div>
           })}
         </div>
@@ -37,7 +37,9 @@ const DataTable = ({ tableTitle }) => {
           {dataColumns.map((item, index) => {
             return <div
               key={index} className={['text-select-color flex justify-between items-center h-20', titleColumns[index].colWidth ? titleColumns[index].colWidth : 'flex-1', index !== 0 ? 'border-l border-word-gray' : ''].join(" ")}>
-              <div className={['flex pl-2 items-center overflow-hidden', titleColumns[index].colWidth ? titleColumns[index].colWidth : 'flex-1',].join(" ")}>{item.content}</div>
+              <div className={['flex pl-2 items-center overflow-hidden', titleColumns[index].colWidth ? titleColumns[index].colWidth : 'flex-1',].join(" ")}>
+                {item.content}
+              </div>
               {titleColumns[index].canCopy &&
                 <div className='h-full flex flex-col justify-start pr-2 pt-1'>
                   <div className='icon iconfont icon-copy1 text-copy-icon cursor-pointer' style={{ fontSize: '16px' }}></div>
