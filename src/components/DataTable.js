2d3e2332ea7d1cf55f853d49745f68b16da0bfb3
SunburstMeoga <1025113986@qq.com>
2024-04-07 16:22:58 +0800

    table components dta

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import { FilterAddress } from '../utils/format'
 const DataTable = ({ titleColumns, dataColumns }) => {
-  // const dataColumns = ['', '', '', '']
+  const handleCopyText = (value) => {
+    console.log(value)
+  }
   return (
     <div>
       <div className=''>
@@ -21,10 +23,10 @@ const DataTable = ({ titleColumns, dataColumns }) => {
               return <div
                 key={index} className={['flex text-select-color justify-between items-center h-16', titleColumns[index].colWidth ? titleColumns[index].colWidth : 'flex-1', index !== 0 ? 'border-l border-word-gray' : ''].join(" ")}>
                 <div className={['flex pl-2 items-center overflow-hidden break-words break-all', titleColumns[index].colWidth ? titleColumns[index].colWidth : 'flex-1'].join(" ")}>
-                  {_item.height}
+                  {titleColumns[index].filterAddress ? FilterAddress(_item[item.flag]) : (item.flag ? _item[item.flag] : '')}
                 </div>
                 {titleColumns[index].canCopy &&
-                  <div className='h-full flex flex-col justify-start pr-2 ml-4 pt-1'>
+                  <div className='h-full flex flex-col justify-start pr-2 ml-4 pt-1' onClick={() => handleCopyText(_item[item.flag])}>
                     <div className='icon iconfont icon-copy2 text-copy-icon cursor-pointer pointer-hover' style={{ fontSize: '18px' }}></div>
                   </div>
                 }
@@ -32,6 +34,23 @@ const DataTable = ({ titleColumns, dataColumns }) => {
             })}
           </div>
         })}
+
+        {/* <div className='flex justify-between items-center border-b border-word-gray'>
+          {titleColumns.map((item, index) => {
+            return <div
+              key={index} className={['flex text-select-color justify-between items-center h-16', titleColumns[index].colWidth ? titleColumns[index].colWidth : 'flex-1', index !== 0 ? 'border-l border-word-gray' : ''].join(" ")}>
+              <div className={['flex pl-2 items-center overflow-hidden break-words break-all', titleColumns[index].colWidth ? titleColumns[index].colWidth : 'flex-1'].join(" ")}>
+                sdf
+              </div>
+              {titleColumns[index].canCopy &&
+                <div className='h-full flex flex-col justify-start pr-2 ml-4 pt-1'>
+                  <div className='icon iconfont icon-copy2 text-copy-icon cursor-pointer pointer-hover' style={{ fontSize: '18px' }}></div>
+                </div>
+              }
+            </div>
+          })}
+        </div> */}
+
       </div>
     </div>
   )
--- src/pages/LastestBlock/index.js
+++ src/pages/LastestBlock/index.js
@@ -5,30 +5,24 @@ import { getListBlock } from '@/api/homeApi';
 
 const LastestBlock = () => {
   const titleColumnsData = [
-    { title: 'Block number', titleWidth: 'w-24', colWidth: '', canCopy: false },
-    { title: 'Block height', titleWidth: 'w-24', colWidth: '', canCopy: false },
-    { title: 'Block timeslot', titleWidth: 'w-24', colWidth: '', canCopy: false },
-    { title: 'Block Hash', titleWidth: '', colWidth: 'w-44', canCopy: true },
-    { title: 'Verify address', titleWidth: '', colWidth: 'w-64', canCopy: true },
-    { title: 'Block reward', titleWidth: '', colWidth: '', canCopy: false },
-    { title: 'Number of transactions', titleWidth: '', colWidth: '', canCopy: false },
-    { title: 'Time', titleWidth: '', colWidth: '', canCopy: false }]
+    { title: 'Block number', titleWidth: 'w-24', colWidth: '', canCopy: false, flag: 'number' },
+    { title: 'Block height', titleWidth: 'w-24', colWidth: '', canCopy: false, flag: 'height' },
+    { title: 'Block timeslot', titleWidth: 'w-24', colWidth: '', canCopy: false, flag: 'slot' },
+    { title: 'Block Hash', titleWidth: '', colWidth: 'w-44', canCopy: true, filterAddress: true, flag: 'hash' },
+    { title: 'Verify address', titleWidth: '', colWidth: 'w-64', canCopy: true, flag: 'mintaddress' },
+    { title: 'Block reward', titleWidth: '', colWidth: '', canCopy: false, flag: 'reward' },
+    { title: 'Number of transactions', titleWidth: '', colWidth: '', canCopy: false, flag: 'txcount' },
+    { title: 'Time', titleWidth: '', colWidth: '', canCopy: false, flag: 'time' }]
   let [dataColumns, changeDataColumns] = useState([])
   let [titleColumns, changeTitleColumns] = useState(titleColumnsData)
   useEffect(() => {
     fetchListBlock()
   }, [])
-
   const fetchListBlock = async () => {
     try {
       const listblock = await getListBlock({ "jsonrpc": "2.0", "method": "listblock", "params": { "fork": "202" }, "id": 83 })
       console.log(listblock)
       changeDataColumns(dataColumns = listblock.data.result)
-      const updataTitleColumns = titleColumns.map(item => {
-        return { ...item, id: 'test id' }
-      })
-      changeTitleColumns(updataTitleColumns)
-      console.log(titleColumns)
     } catch (err) {
       console.log(err)
     }
