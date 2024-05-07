3a102714bdcfa03cf8207d1bf02f5fdae9fb47be
SunburstMeoga <1025113986@qq.com>
2024-05-07 18:05:22 +0800

    mobile

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -19,28 +19,28 @@ const DataTable = ({ titleColumns, dataColumns }) => {
   return (
     <div>
       {contextHolder}
-      <div className='px-1-4 pb-3-8'>
-        <div className='flex justify-between items-center border-b-4 border-line-gray'>
+      <div className='px-1-4 pb-3-8 overflow-x-scroll lg:overflow-auto bg-white '>
+        <div className='flex justify-between items-center lg:border-b-4 border-line-gray'>
           {titleColumns.map((item, index) => {
             return <div
               key={index}
-              className={['', item.colWidth ? item.colWidth : 'flex-1', index !== 0 ? 'border-l border-word-gray' : ''].join(" ")}>
-              <div className={['pl-0-4 flex justify-start items-center h-6-2 text-1-4 font-semibold', item.titleWidth ? item.titleWidth : ''].join(" ")}>{item.title}</div>
+              className={['', item.colWidth ? item.colWidth : 'flex-1', index !== 0 ? 'lg:border-l lg:border-word-gray' : ''].join(" ")}>
+              <div className={['pl-0-4 flex justify-start items-center h-6-2 text-1-4 font-bold lg:font-semibold', item.colWidth ? item.colWidth : ''].join(" ")}>{item.title}</div>
             </div>
           })}
         </div>
-
+        <div className='w-99-9 bg-black h-0-1 lg:hidden'></div>
         {dataColumns.map((_item, _index) => {
-          return <div key={_index} className='flex justify-between items-center border-b border-word-gray item-hover cursor-pointer'
+          return <div key={_index} className='flex w-99-9 lg:w-full justify-between items-center border-b border-word-gray item-hover cursor-pointer'
             onClick={() => { navigate('/block-details/' + _item.hash) }}>
             {titleColumns.map((item, index) => {
               return <div
-                key={index} className={['flex text-select-color justify-between items-center h-6-4', titleColumns[index].colWidth ? titleColumns[index].colWidth : 'flex-1', index !== 0 ? 'border-l border-word-gray' : ''].join(" ")}>
+                key={index} className={['flex text-select-color justify-between items-center h-6-4', titleColumns[index].colWidth ? titleColumns[index].colWidth : 'flex-1', index !== 0 ? 'lg:border-l  border-word-gray' : ''].join(" ")}>
                 <div className={['flex pl-0-4 items-center overflow-hidden break-words break-all text-1-5', titleColumns[index].colWidth ? titleColumns[index].colWidth : 'flex-1'].join(" ")}>
                   {titleColumns[index].filterAddress ? FilterAddress(_item[item.flag]) : (item.flag ? _item[item.flag] : '')}
                 </div>
                 {titleColumns[index].canCopy &&
-                  <div className='h-full flex flex-col justify-start pr-1-0 ml-1-0 pt-1-0' onClick={() => handleCopyText(_item[item.flag])}>
+                  <div className='h-full flex flex-col justify-start pr-1-0 lg:ml-1-0 pt-1-0' onClick={() => handleCopyText(_item[item.flag])}>
                     <div className='icon iconfont icon-copy2 text-copy-icon cursor-pointer pointer-hover' style={{ fontSize: '18px' }}></div>
                   </div>
                 }
--- src/pages/LastestBlock/index.js
+++ src/pages/LastestBlock/index.js
@@ -7,16 +7,16 @@ import { getListBlock } from '@/api/homeApi';
 const LastestBlock = () => {
 
   const titleColumnsData = [
-    { title: 'Block Number', titleWidth: 'w-24', colWidth: 'w-8-0', canCopy: false, flag: 'number' },
-    { title: 'Block Height', titleWidth: 'w-24', colWidth: 'w-8-0', canCopy: false, flag: 'height' },
-    { title: 'Block Timeslot', titleWidth: 'w-24', colWidth: 'w-8-0', canCopy: false, flag: 'slot' },
-    { title: 'Block Hash', titleWidth: '', colWidth: 'w-15-2', canCopy: true, filterAddress: true, flag: 'hash' },
-    { title: 'Verify Address', titleWidth: '', colWidth: 'w-24-1', canCopy: true, flag: 'mintaddress' },
-    { title: 'Block Teward', titleWidth: '', colWidth: 'w-14-4', canCopy: false, flag: 'reward' },
-    { title: 'Number Of Transactions', titleWidth: '', colWidth: 'w-9-6', canCopy: false, flag: 'txcount' },
-    { title: 'Inscription Trading', titleWidth: '', colWidth: 'w-9-9', canCopy: false, flag: 'txcount' },
+    { title: 'Block Number', titleWidth: 'w-24', colWidth: 'w-7-4  lg:w-8-0', canCopy: false, flag: 'number' },
+    { title: 'Block Height', titleWidth: 'w-24', colWidth: 'w-7-4 lg:w-8-0', canCopy: false, flag: 'height' },
+    { title: 'Block Timeslot', titleWidth: 'w-24', colWidth: 'w-7-4 lg:w-8-0', canCopy: false, flag: 'slot' },
+    { title: 'Block Hash', titleWidth: '', colWidth: 'w-15-4 mr-2-0 lg:pr-0-1 lg:w-15-2', canCopy: true, filterAddress: true, flag: 'hash' },
+    { title: 'Verify Address', titleWidth: '', colWidth: 'w-25-4 lg:pr-0-1 lg:w-24-1', canCopy: true, flag: 'mintaddress' },
+    { title: 'Block Teward', titleWidth: '', colWidth: 'w-7-4 lg:w-14-4', canCopy: false, flag: 'reward' },
+    { title: 'Number Of Transactions', titleWidth: '', colWidth: 'w-10-4 lg:pr-0-1 lg:w-9-6', canCopy: false, flag: 'txcount' },
+    { title: 'Inscription Trading', titleWidth: '', colWidth: 'w-8-4 lg:w-9-9', canCopy: false, flag: 'txcount' },
 
-    { title: 'Time', titleWidth: '', colWidth: 'w-12-2', canCopy: false, flag: 'time' }]
+    { title: 'Time', titleWidth: '', colWidth: 'w-10-2', canCopy: false, flag: 'time' }]
   let [dataColumns, changeDataColumns] = useState([])
   useEffect(() => {
     fetchListBlock()
@@ -33,19 +33,19 @@ const LastestBlock = () => {
   return (
     <div className='bg-primary-green w-full min-h-svh '>
       <div className='w-full flex flex-col justify-start items-center '>
-        <div className='font-bold  module-title cursor-pointer pl-5-9 w-full mt-5-5 text-8-0'>
+        <div className='font-light lg:font-bold py-2-5 lg:py-0-1 module-title cursor-pointer pl-1-3 lg:pl-5-9 w-full lg:mt-5-5 text-4-0 lg:text-8-0'>
           Latest Block
         </div>
-        <div className='w-full flex justify-end pr-7-8 mb-0-7'>
+        <div className='w-full hidden lg:flex justify-end pr-7-8 mb-0-7 '>
           <PageSize />
         </div>
-        <div className='px-3-0 w-full mb-3-3'>
-          <div className='bg-white w-full rounded-3xl pt-1-7 shadow-2xl'>
+        <div className='lg:px-3-0 w-full lg:mb-3-3'>
+          <div className='bg-white w-full lg:rounded-3xl lg:pt-1-7 shadow-2xl'>
             <DataTable titleColumns={titleColumnsData} dataColumns={dataColumns} />
           </div>
         </div>
 
-        <div className='w-full flex justify-end mb-7-0 pr-7-8'>
+        <div className='w-full hidden lg:flex justify-end mb-7-0 pr-7-8'>
           <Pagination showJump />
         </div>
       </div>
