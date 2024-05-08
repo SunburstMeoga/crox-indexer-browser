0d31294efe548b460efd2a9c4a9c6ddfe0ab7212
SunburstMeoga <1025113986@qq.com>
2024-05-08 17:13:21 +0800

    fat

--- src/pages/BRC20Details/transferDataDetails.js
+++ src/pages/BRC20Details/transferDataDetails.js
@@ -41,8 +41,8 @@ const TransferDataDetails = () => {
         getTransHashDetails()
     }, [])
     const getTransferDetails = async () => {
-        // let detailsInfo = await getBrc20TransferDetails({ "jsonrpc": "2.0", "method": "gettransaction", "params": { "txid": id, "fork": "202" }, "id": 83 })
-        // console.log(detailsInfo)
+        let detailsInfo = await getBrc20TransferDetails({ "jsonrpc": "2.0", "method": "gettransaction", "params": { "txid": btctxId, "fork": "202" }, "id": 83 })
+        console.log(detailsInfo)
     }
     const getTransHashDetails = async () => {
         let res = await getHashDetails({ "jsonrpc": "2.0", "method": "getbtctxinfo", "params": { "btctxid": btctxId }, "id": 83 })
@@ -79,44 +79,44 @@ const TransferDataDetails = () => {
             title: 'Date:', content: 'null'
         }])
         getHashInfo(hashInfo = res.data.result)
-        console.log(res)
+        console.log(hashInfo)
     }
     return (
         <div>
             <div className='bg-primary-green w-full flex flex-col items-center justify-start min-h-svh'>
-                <div className='pl-6-9 text-6-0 text-module-title w-full font-medium mt-6-6'>
+                <div className='pl-1-0 lg:pl-6-9 text-2-0 lg:text-6-0 text-module-title w-full font-medium mt-2-0 lg:mt-6-6 mb-1-0 lg:mb-auto'>
                     Trade
                 </div>
-                <div className='pl-6-9 text-6-0 text-module-title w-full font-bold flex justify-start items-baseline'>
-                    <div className='text-12-5 font-bold'>Hash</div>
-                    <div className='pl-0-8 text-2-0 font-medium'>{getHashInfo.txtime}</div>
+                <div className='pl-1-0 lg:pl-6-9 text-1-0 lg:text-6-0 text-module-title w-full font-bold lg:flex justify-start items-baseline'>
+                    <div className='text-4-0 lg:text-12-5 font-bold'>HAH</div>
+                    <div className='lg:pl-0-8 lg:text-2-0 text-1-0 font-medium'>{hashInfo.txtime}</div>
                 </div>
-                <div className='w-full px-6-9 flex justify-between items-center mb-2'>
+                <div className='w-full px-1-0 lg:px-6-9 lg:flex justify-between items-center mt-1-0 lg:mt-auto'>
                     {cardList.map((item, index) => {
-                        return <div key={index} className="w-34-4 py-2-6 pl-2-9 mb-1-3  rounded-2xl overflow-hidden e cursor-pointer bg-card-green ">
+                        return <div key={index} className="lg:w-34-4 py-2-6 pl-2-9 mb-1-3  rounded-2xl overflow-hidden e cursor-pointer bg-card-green ">
                             <DataCard title={item.title} quantities={item.quantities} cardStatus={item.cardStatus} />
                         </div>
                     })}
                 </div>
-                <div className='flex justify-between items-center w-full px-6-9 mb-5-6'>
-                    <div className='py-2-0 px-3-4 bg-module-title rounded-2xl w-57-0'>
+                <div className='lg:flex justify-between items-center w-full lg:px-6-9 lg:mb-5-6'>
+                    <div className='lg:py-2-0 lg:px-3-4 bg-module-title lg:rounded-2xl w-full mb-0-4 lg:mb-auto lg:w-57-0'>
                         {inforList.map((item, index) => {
-                            return <div key={index} className={['h-4-9 w-full pl-1-1 flex justify-start items-center', index != inforList.length - 1 ? 'border-b  border-trans-gray' : ''].join(' ')}>
-                                <div className='font-semibold text-select-color w-22-8'>{item.title}</div>
+                            return <div key={index} className={['h-5-2 lg:h-4-9 lg:w-full pl-1-1 flex justify-start items-center', index != inforList.length - 1 ? 'border-b  border-trans-gray' : ''].join(' ')}>
+                                <div className='font-semibold text-select-color w-9-9 lg:w-22-8'>{item.title}</div>
                                 <div className='font-medium text-white pl-2-5 h-full flex justify-start items-center border-l border-trans-gray'>{item.content}</div>
                             </div>
                         })}
                     </div>
                     <div className='flex flex-col justify-between items-center'>
                         {detailsCard.map((item, index) => {
-                            return <div key={index} className={['px-2-8 py-2-1 bg-module-title w-47-0 h-10-0 rounded-2xl', index !== detailsCard.length - 1 ? 'mb-1-0' : ''].join(" ")}>
+                            return <div key={index} className={['px-2-0 py-1-6 lg:px-2-8 lg:py-2-1 bg-module-title w-full lg:w-47-0 h-11-0 lg:h-10-0 lg:rounded-2xl', index !== detailsCard.length - 1 ? 'mb-0-4 lg:mb-1-0' : ''].join(" ")}>
                                 <div className='flex justify-between items-center w-full'>
-                                    <div className='text-1-5 font-semibold text-select-color'>{item.title}</div>
+                                    <div className='text-1-0 lg:text-1-5 font-semibold text-select-color'>{item.title}</div>
                                     <div className=''>
-                                        <div className='icon iconfont icon-copy2 text-primary-green cursor-pointer' style={{ fontSize: '2.4rem' }}></div>
+                                        <div className='icon iconfont icon-copy2 text-primary-green cursor-pointer' style={{ fontSize: '2rem' }}></div>
                                     </div>
                                 </div>
-                                <div className='w-full text-white mt-1-2 break-words text-1-5 font-medium'>
+                                <div className='w-17-3 lg:w-full text-white mt-1-2 break-words text-1-0 lg:text-1-5 font-medium'>
                                     {item.content}
                                 </div>
                             </div>
